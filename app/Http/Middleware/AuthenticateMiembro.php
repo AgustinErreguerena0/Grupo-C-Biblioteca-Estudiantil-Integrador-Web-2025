57a88d858f8cb63cf994e2dcfe3da36a0e30abf6
FJP760 <fpiloni76@gmail.com>
2025-06-30 10:15:29 -0300

    login bibliotecario y usuario, middleware funcionando, error 419 vigente

--- app/Http/Middleware/AuthenticateMiembro.php
+++ app/Http/Middleware/AuthenticateMiembro.php
@@ -5,13 +5,17 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log; // Add this line
 
 class AuthenticateMiembro
 {
     public function handle(Request $request, Closure $next)
     {
-        if (!Auth::guard('miembro')->check()) { //
-            return redirect()->route('login'); //
+        Log::info("AuthenticateMiembro: Checking if miembro is authenticated."); // Add this line
+
+        if (!Auth::guard('miembro')->check()) {
+            Log::info("AuthenticateMiembro: Miembro not authenticated, redirecting to login."); // Add this line
+            return redirect()->route('login');
         }
 
         return $next($request);
